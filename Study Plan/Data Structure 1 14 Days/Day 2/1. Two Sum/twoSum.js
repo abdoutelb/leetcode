507aba036bc3701f46b238985662ef541d592ceb
abdoutelb <abdoutelb.fci@gmail.com>
2021-10-05 04:07:54 +0200

    add TwoSum from day 2

---
+++ Study Plan/Data Structure 1 14 Days/Day 2/1. Two Sum/twoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    let result = new Map();
+    result.set(nums[0],0);
+    for(let i=1;i<nums.length;i++){ 
+        if(result.has(target - nums[i])){
+            return [result.get(target - nums[i]),i];
+        }else{
+            result.set(nums[i],i);
+        }
+    }
+    return false;
+    
+};
